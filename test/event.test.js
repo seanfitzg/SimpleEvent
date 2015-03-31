f5739d6e984353ad7933fabdc2cd8694126319d4
Sean Fitzgerald <Sean@Seans-MacBook-Pro.local>
2015-03-31 23:59:40 +0100

    can build an object from an event with multiple properties

--- test/event.test.js
+++ test/event.test.js
@@ -7,60 +7,67 @@ var assert = require("assert");
 
 
 describe('Simples basic functions', function () {
-    var events;
+  var events;
+  var newObj;
+  var es = new Simples();
+
+  beforeEach(function() {
+    newObj = {}
+    events = [ { name: 'Sean' } ];
+  });
+
+  it('builds a simple object', function () {
+    es.replay(newObj, events);
+    newObj.name.should.equal('Sean');
+  });
+
+  it('does not throw an error if the events object is undefined', function () {
+    events = undefined;
+    es.replay(newObj, events);
+  });
+
+  it('throws an error if root object is undefined', function () {
     var newObj;
-    var es = new Simples();
-    
-    beforeEach(function() {
-        newObj = {}
-        events = [ { name: 'Sean' } ];
-    });
-    
-    it('builds a simple object', function () {
-        es.replay(newObj, events);
-        newObj.name.should.equal('Sean');
-    });
-    
-    it('does not throw an error if the events object is undefined', function () {
-        events = undefined;
-        es.replay(newObj, events);
-    });
-    
-    it('throws an error if root object is undefined', function () {
-        var newObj;
-        assert.throws(function() {
-            es.replay(newObj, events)
-        }, Error);
-    });
-    
-    it('builds an object from multiple events', function() {
-        events.push({address: 'Limerick'});
-        es.replay(newObj, events);
-        newObj.name.should.equal('Sean');
-        newObj.address.should.equal('Limerick');
-    });
-    
+    assert.throws(function() {
+      es.replay(newObj, events)
+    }, Error);
+  });
+
+  it('builds an object from multiple events', function() {
+    events.push({address: 'Limerick'});
+    es.replay(newObj, events);
+    newObj.name.should.equal('Sean');
+    newObj.address.should.equal('Limerick');
+  });
+
+  it('builds an object from an event with multiple properties', function () {
+    events[0].age = 41;
+    es.replay(newObj, events);
+    newObj.name.should.equal('Sean');
+    newObj.age.should.equal(41);
+  })
 })
 
 describe('Object built from descriptive event names', function() {
 
-    var events;
-    var newObj;
-    var es = new Simples({
-        usePrefix: true
-    });
-        
-    beforeEach(function() {
-        newObj = {}
-        events = [ { nameChanged: 'Sean' } ];
-    });  
-    
-    it('the usePrefix config option is true', function() {
-       es.usePrefix.should.equal(true); 
-    });
-    
-    it('uses the event name prefix as the property', function() {
-        es.replay(newObj, events);
-        newObj.name.should.equal('Sean');
-    });
+  var events;
+  var newObj;
+  var es = new Simples({
+    usePrefix: true
+  });
+
+  beforeEach(function() {
+    newObj = {}
+    events = [ { nameChanged: 'Sean' } ];
+  });  
+
+  it('the usePrefix config option is true', function() {
+   es.usePrefix.should.equal(true); 
+ });
+
+  it('uses the event name prefix as the property', function() {
+    es.replay(newObj, events);
+    newObj.name.should.equal('Sean');
+  });
 });
+
